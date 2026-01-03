814c77455bb49a9d059fee913160d34d9c9053fe
Programiso <dominikptasinski388@gmail.com>
2026-01-03 20:09:13 +0100

    Refactor text_ui to support Polish language

--- Password Creater/UI.py
+++ Password Creater/UI.py
@@ -1,22 +1,41 @@
-def text_ui(program_name="PassPy", version_string="Beta 1.1", ):
-    options = ["[1] Change settings",
-               "[2] Create password with assistant",
-               "[3] Check security of password",
-               "[4] Exit"
-               ]
+def text_ui(language, program_name="PassPy", version_string="Beta 1", ):
+    """Funkcja wypisujaca UI
+    Wymagane parametry: language
+    Opcjonalne parametry: program_name, version_string"""
+    if language == 1: #wsparcie dla Angielskiego
+        options = ("[1] Change settings",
+                   "[2] Create password with assistant",
+                   "[3] Check security of password",
+                   "[4] Exit"
+                   )
+    elif language == 0: #wsparcie dla Polskiego
+        options = ("[1] Zmień ustawienia",
+                   "[2] Utwórz hasło z kreatorem",
+                   "[3] Sprawdź bezpieczeństwo hasła",
+                   "[4] Wyjdź")
+
     #pokaz komunikat
-    print(f"Welcome to {program_name} version {version_string}.\nChoose an action: \n{options[0]}\n{options[1]}\n{options[2]}\n{options[3]}")
+    if language == 1: #wsparcie dla Angielskiego
+        print(f"{program_name} {version_string}.(C)Programiso 2026\nChoose an action: \n{options[0]}\n{options[1]}\n{options[2]}\n{options[3]}")
+    elif language == 0:
+        print(f"{program_name} {version_string}.(C)Programiso 2026\nWybierz akcję: \n{options[0]}\n{options[1]}\n{options[2]}\n{options[3]}")
+
     while True:
         choose = input(">")
-        try:
-            choose = int(choose)
-        except ValueError:
-            print(f"The option you've entered is invalid, please choose from  1 to {len(options)}")
         
-        if choose in range(1,len(options) + 1):
+        try: #proba przekonwertowania
+            choose = int(choose)
+        except ValueError: #jezeli typ jest inny
+            if language == 1: #wsparcie dla Angielskiego
+                print(f"The option you entered is invalid. Please enter an option between 1 and {len(options)}.")
+            if language == 0: #wsparcie dla Polskiego
+                print(f"Podana opcja nie jest prawidłowa. Podaj opcję pomiędzy 1 a {len(options)}")
+        if choose in range(1,len(options) + 1): 
             return choose
         else:
-            print("Please choose an option in range of 1 to 4")
-
+            if language == 1: #wsparcie dla Angielskiego
+                print("Please choose an option in range of 1 to 4")
+            elif language == 0: #wsparcie dla Polskiego
+                print("Wybierz opcję pomiędzy 1 a 4")
         
 
